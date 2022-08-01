d0f46ccc436f9be61071b809b0dbdff19c90b3ed
Jalal Abboud <73166985+jalal-git@users.noreply.github.com>
2022-08-01 11:12:37 +0300

    Update test_kafka.py

--- test_kafka.py
+++ test_kafka.py
@@ -6,27 +6,12 @@ spark = SparkSession \
         .config("spark.sql.debug.maxToStringFields", "100") \
         .getOrCreate()
 
-kafka_df = spark.readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9009") \
-    .option("kafka.security.protocol", "SSL") \
-    .option("failOnDataLoss", "false") \
-    .option("subscribe", "topic1") \
-    .option("includeHeaders", "true") \
-    .option("startingOffsets", "latest") \
-    .option("spark.streaming.kafka.maxRatePerPartition", "50") \
-    .load()
+df = spark.readStream
+      .format("socket")
+      .option("host","localhost")
+      .option("port","9009")
+      .load()
 
-def func_call(df, batch_id):
-    df.selectExpr("CAST(value AS STRING) as json")
-    requests = df.rdd.map(lambda x: x.value).collect()
-    df.show()
-    logging.info(requests)
-    
-    
-query = kafka_df.writeStream \
-    .format(HiveWarehouseSession.STREAM_TO_STREAM) \
-    .foreachBatch(func_call) \
-    .option("checkpointLocation","file://F:/tmp/kafka/checkpoint") \
-    .trigger(processingTime="5 seconds") \
-    .start().awaitTermination()
+query = (df.show().writeStream.outputMode("complete").format("console").queryName("counts").start())
+query.awaitTermination() 
+spark.stop()
