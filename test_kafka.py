3c44c310a8c025766788b69f6ff56707748a59f7
Jalal Abboud <73166985+jalal-git@users.noreply.github.com>
2022-08-01 10:56:25 +0300

    Update test_kafka.py
    commit

--- test_kafka.py
+++ test_kafka.py
@@ -1,24 +1,32 @@
 from pyspark.sql import SparkSession
 
-# create spark session
 spark = SparkSession \
-    .builder \
-    .appName("StructuredStreamingKafkaExample") \
-    .getOrCreate()
+        .builder \
+        .appName("test") \
+        .config("spark.sql.debug.maxToStringFields", "100") \
+        .getOrCreate()
 
-# create kafka streaming
-kafkaDF = (spark
-  .readStream
-  .option("kafka.bootstrap.servers", "localhost:9009")
-  .option("subscribe", "en")
-  .format("kafka")
-  .load()
-)
+kafka_df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9009") \
+    .option("kafka.security.protocol", "SSL") \
+    .option("failOnDataLoss", "false") \
+    .option("subscribe", "topic1") \
+    .option("includeHeaders", "true") \
+    .option("startingOffsets", "latest") \
+    .option("spark.streaming.kafka.maxRatePerPartition", "50") \
+    .load()
 
-query = kafkaDF \
-    .writeStream \
-    .outputMode("complete") \
-    .format("console") \
-    .start()
-query.start()
-query.awaitTermination()
+def func_call(df, batch_id):
+    df.selectExpr("CAST(value AS STRING) as json")
+    requests = df.rdd.map(lambda x: x.value).collect()
+    df.show()
+    logging.info(requests)
+    
+    
+query = kafka_df.writeStream \
+    .format(HiveWarehouseSession.STREAM_TO_STREAM) \
+    .foreachBatch(func_call) \
+    .option("checkpointLocation","file://F:/tmp/kafka/checkpoint") \
+    .trigger(processingTime="5 seconds") \
+    .start().awaitTermination()
