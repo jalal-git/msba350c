29cf8a51926cdf465dc0e935dc6d5541dce6cdd7
Ubuntu <azureuser@app-vm.ffby5zhjwaluvh1tyehlz501ue.lx.internal.cloudapp.net>
2022-08-01 07:30:46 +0000

    removed some files

--- test_socket_server.py
+++ test_socket_server.py
@@ -2,18 +2,18 @@ import socket
 import time
 import json
 
-HOST = '127.0.0.1'
-PORT = 5050
+HOST = 'localhost'
+PORT = 9009
 print(socket.gethostbyname(HOST))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
-    with conn:
-        print(f"Connected by { addr}")
-        data = conn.recv(1024)
-        print(data.decode())
-        conn.sendall("Hello World!".encode())
-        # data = conn.send("Hello World".encode)
+    print(f"Connected by { addr}")
+    data = conn.recv(1024)
+    print(data.decode())
+    conn.sendall("Hello World!".encode())
+    print('sent')
+    # data = conn.send("Hello World".encode)
     
--- test_stream.py
+++ test_stream.py
@@ -4,9 +4,9 @@ import time
 
 # Create a local StreamingContext with two working thread and batch interval of 1 second
 sc = SparkContext("local[2]", "NetworkWordCount")
-ssc = StreamingContext(sc, 2)
+ssc = StreamingContext(sc, 10)
 
-lines = ssc.socketTextStream("127.0.0.1", 5050)
+lines = ssc.socketTextStream("localhost",9009)
 words = lines.flatMap(lambda line: line.split(" "))
 pairs = words.map(lambda word: (word, 1))
 wordCounts = pairs.reduceByKey(lambda x, y: x + y)
@@ -16,3 +16,4 @@ wordCounts.pprint()
 print(pairs)
 ssc.start()
 ssc.awaitTermination()
+ssc.stop()
