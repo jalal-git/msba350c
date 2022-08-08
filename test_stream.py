2edb172d80326f59f6a9c5c712f40ad26866da33
Jalal Abboud <73166985+jalal-git@users.noreply.github.com>
2022-08-08 20:08:47 +0300

    Update test_stream.py
    m

--- test_stream.py
+++ test_stream.py
@@ -1,12 +1,21 @@
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 import time
 
+# create spark configuration
+conf = SparkConf()
+conf.setAppName("StreamApp")
+
 # Create a local StreamingContext with two working thread and batch interval of 1 second
-sc = SparkContext("local[2]", "NetworkWordCount")
+# create spark instance with the above configuration
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
 ssc = StreamingContext(sc, 10)
+# add checkpoint
+ssc.checkpoint("checkpoint_App")
 
 lines = ssc.socketTextStream("localhost",9009)
+print("connected!")
 words = lines.flatMap(lambda line: line.split(" "))
 pairs = words.map(lambda word: (word, 1))
 wordCounts = pairs.reduceByKey(lambda x, y: x + y)
