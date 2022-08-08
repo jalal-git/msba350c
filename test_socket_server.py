b8b9123ef64bcca598501e0fb2764e06a21afd32
Jalal Abboud <73166985+jalal-git@users.noreply.github.com>
2022-08-08 20:26:24 +0300

    Update test_socket_server.py

--- test_socket_server.py
+++ test_socket_server.py
@@ -8,10 +8,10 @@ PORT = 9009
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+    print("Waiting for connection ...")
+    conn, addr = s.accept()
+    print(f"Connected by { addr}")
     while True:
-        print("Waiting for connection ...")
-        conn, addr = s.accept()
-        print(f"Connected by { addr}")
         conn.sendall("""{"1":1, "2":2}""".encode())
         print('sent')
         # data = conn.send("Hello World".encode)
