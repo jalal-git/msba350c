7e9b22fc952de3d8ad3fce70cc557034713ca9f9
Jalal Abboud <73166985+jalal-git@users.noreply.github.com>
2022-08-08 20:34:00 +0300

    Update test_socket_server.py
    m

--- test_socket_server.py
+++ test_socket_server.py
@@ -12,7 +12,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     print(f"Connected by { addr}")
     while True:
-        conn.sendall("""{"1":1, "2":2}""".encode())
+        conn.send("""{"1":1, "2":2}""".encode())
         print('sent')
         # data = conn.send("Hello World".encode)
     
