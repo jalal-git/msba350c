1526f12fab2ceb4bd93355846c484aae5f63aec3
Jalal Abboud <73166985+jalal-git@users.noreply.github.com>
2022-08-08 19:58:12 +0300

    Update test_socket_server.py

--- test_socket_server.py
+++ test_socket_server.py
@@ -4,11 +4,11 @@ import json
 
 HOST = 'localhost'
 PORT = 9009
-print(socket.gethostbyname(HOST))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+    print("Waiting for connection ...")
     conn, addr = s.accept()
     print(f"Connected by { addr}")
     data = conn.recv(1024)
