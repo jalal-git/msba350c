2c63e788a4929e6efeaf2655bc651b624fed35af
Jalal Abboud <73166985+jalal-git@users.noreply.github.com>
2022-08-09 20:25:02 +0300

    Update test_socket_server.py

--- test_socket_server.py
+++ test_socket_server.py
@@ -14,6 +14,9 @@ creds = creds.readlines()
 api_key = creds[0].lstrip('API Key:').rstrip('\n').strip()
 api_secret = creds[1].lstrip('Secret Key:').rstrip('\n').strip()
 
+# field names
+names = ['open time', 'open', 'high', 'low', 'close', 'volume', 'close time', 'quote asset volume', 'number of trades', 'taker buy base asset volume', 'taker buy quote asset volume', 'ignore']
+
 # init
 client = Client(api_key, api_secret)
 
@@ -21,19 +24,17 @@ client = Client(api_key, api_secret)
 HOST = 'localhost'
 PORT = 9009
 
-data = []
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
     
     while True:
-        tick = client.get_symbol_ticker(symbol="BTCUSDT")
-        data.append(tick)
+        ticks = requests.get('https://api.binance.com/api/v1/klines?symbol=BTCUSDT&interval=1m').json()
+        
         print("Waiting for connection ...")
         conn, addr = s.accept()
         print(f"Connected by {addr}")
-        conn.send(str(data).encode())
+        conn.send(str([{names[i]:elem[i] for i in range(12)} for elem in ticks]).encode())
         conn.close()
         print('sent')
         time.sleep(10)
