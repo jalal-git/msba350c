d1283289b029fdd5b944b4c7a77983bd8ac7dbcb
Jalal Abboud <73166985+jalal-git@users.noreply.github.com>
2022-08-09 15:16:33 +0300

    Update test_socket_server.py

--- test_socket_server.py
+++ test_socket_server.py
@@ -1,7 +1,22 @@
 import socket
 import time
-import json
+from binance.client import Client
+import time
+import warnings
+warnings.filterwarnings('ignore')
 
+# reading credentials file
+creds = open('creds.txt')
+creds = creds.readlines()
+
+# getting api key and secret
+api_key = creds[0].lstrip('API Key:').rstrip('\n').strip()
+api_secret = creds[1].lstrip('Secret Key:').rstrip('\n').strip()
+
+# init
+client = Client(api_key, api_secret)
+
+# configuring socket communication 
 HOST = 'localhost'
 PORT = 9009
 
@@ -12,12 +27,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         print("Waiting for connection ...")
         conn, addr = s.accept()
-        print(f"Connected by { addr}")
-
-        conn.send("""{"1":1, "2":2}""".encode())
+        print(f"Connected by {addr}")
+        conn.send(client.get_symbol_ticker(symbol="BTCUSDT")['price'].encode())
         conn.close()
         print('sent')
         time.sleep(10)
         # data = conn.send("Hello World".encode)
 s.close()
-    
