664c9a6aca7dec7196a02f673c5c4e3264fa7119
Jalal Abboud <73166985+jalal-git@users.noreply.github.com>
2022-08-10 21:06:47 +0300

    Update test_stream_df.py

--- test_stream_df.py
+++ test_stream_df.py
@@ -6,6 +6,11 @@ from pyspark.sql.window import Window
 from pyspark.sql.types import FloatType
 import re
 import json
+import datetime
+
+def to_date(your_timestamp):
+    return str(datetime.datetime.fromtimestamp(your_timestamp))
+
 
 def get_num(string):
     pattern = "\d+\.\d+" if '.' in string else "\d+"
@@ -36,7 +41,8 @@ to_float = f.udf(lambda v: get_num(v), FloatType())
 df = df.select([to_float(c).alias(c) for c in df.columns])
 
 # to timestamp
-df = df.withColumn('open_timestamp',f.to_timestamp("open_time", "yyyy/MM/dd HHmm"))
+df = df.withColumn('open_time_str', f.udf(to_date('open_time'))
+df = df.withColumn('open_timestamp', f.to_timestamp("open_time", "yyyy/MM/dd HHmm"))
 
 # #create window by casting timestamp to long (number of seconds)
 # w = (Window.orderBy(f.col("open_time")).rangeBetween(-1, 0))
