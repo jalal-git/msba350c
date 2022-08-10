3d819ef6655c60b263039ad74c757a21988856a2
Jalal Abboud <73166985+jalal-git@users.noreply.github.com>
2022-08-10 21:12:12 +0300

    Update test_stream_df.py

--- test_stream_df.py
+++ test_stream_df.py
@@ -3,12 +3,12 @@ from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 from pyspark.sql import functions as f
 from pyspark.sql.window import Window
-from pyspark.sql.types import FloatType
+from pyspark.sql.types import *
 import re
 import json
 import datetime
 
-def to_date(your_timestamp):
+def get_date(your_timestamp):
     return str(datetime.datetime.fromtimestamp(your_timestamp))
 
 
@@ -41,8 +41,10 @@ to_float = f.udf(lambda v: get_num(v), FloatType())
 df = df.select([to_float(c).alias(c) for c in df.columns])
 
 # to timestamp
-df = df.withColumn('open_time_str', f.udf(to_date('open_time')))
-df = df.withColumn('open_timestamp', f.to_timestamp("open_time", "yyyy/MM/dd HHmm"))
+to_date = f.udf(lambda v: get_date(v), StringType())
+
+df = df.withColumn('open_time_str', to_date('open_time'))
+df = df.withColumn('open_timestamp', f.to_timestamp("open_time_str", "yyyy/MM/dd HHmm"))
 
 # #create window by casting timestamp to long (number of seconds)
 # w = (Window.orderBy(f.col("open_time")).rangeBetween(-1, 0))
